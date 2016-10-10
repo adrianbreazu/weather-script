cce54c8a60db1da0e6ceaa1b3e630224c13f3aac
Adrian Breazu <breazuadrian@gmail.com>
2016-10-10 23:00:20 +0300

    .

--- readdata.py
+++ readdata.py
@@ -5,15 +5,14 @@ import datetime as datetime
 
 SENSORTYPE = dht_sensor.DHT22
 GPIO_PIN = 17
-#DB = '/var/www/weather-script/weather-data.db'
-DB = '/home/adrianbreazu/repositories/weather-script/weather-data.db'
+DB = '/var/www/weather-script/weather-data.db'
 
 
 def get_readings():
     try:
         humidity, temperature = dht_sensor.read_retry(sensor=SENSORTYPE, pin=GPIO_PIN, platform="RASPBERRY_PI")
         reads = {}
-        connection = sqlite.connect(DB)
+        connection = sqlite3.connect(DB)
         cursor = connection.cursor()
 
         if (humidity is not None) & (temperature is not None):
@@ -22,10 +21,10 @@ def get_readings():
         else:
             print("Exception humidity is None or temperature is None")
 
-        cursor.execute("INSERT INTO temperature_humidity VALUES ( NULL, ?, ?, ?)", (humidity, temperature, str(datetime.datetime.now().strftime("%Y/%M/%D %H:%M:%S"))))
+        cursor.execute("INSERT INTO temperature_humidity VALUES ( NULL, ?, ?, ?)", (humidity, temperature, str(datetime.datetime.now().strftime("%y-%m-%d %H:%M:%S"))))
         connection.commit()
 
-    except sqlite.Error as e:
+    except sqlite3.Error as e:
         print("SQL Error: {0}".format(e.args[0]))
     
     except Exception as e:
